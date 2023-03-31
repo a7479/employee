1d1d2f4822e90af79245178f7a55c63f9be81b99
Александр <perchatki79@gmail.com>
2023-03-31 23:13:24 +0300

    second commit

--- src/main/java/pro/sky/employee/EmployeeService.java
+++ src/main/java/pro/sky/employee/EmployeeService.java
@@ -1,4 +1,52 @@
 package pro.sky.employee;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Service
 public class EmployeeService {
+
+    private final List<Employee> staff = new ArrayList<>();
+
+
+    public Employee addEmployee(String firstName, String lastName) {
+
+        Employee temp = new Employee(firstName, lastName);
+        if (staff.contains(temp)) {
+            throw new EmployeeAlreadyAddedException();
+        }
+
+        staff.add(temp);
+        return temp;
+
+
+    }
+
+    public Employee removeEmployee(String firstName, String lastName) {
+        Employee temp = new Employee(firstName, lastName);
+        if (staff.contains(temp)) {
+            staff.remove(temp);
+            return temp;
+        }
+
+        throw new EmployeeAlreadyAddedException();
+
+
+    }
+
+    public Employee findEmployee(String firstName, String lastName) {
+        Employee temp = new Employee(firstName, lastName);
+        if (staff.contains(temp)) {
+            return temp;
+        }
+        throw new EmployeeNotFoundException();
+    }
+
+    public Collection<Employee> findAll() {
+        return new ArrayList<>(staff);
+    }
 }
